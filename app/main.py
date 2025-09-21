e3a5d7112ed99a81586dfa53f13403837c7accbc
shobhit <jajooshobhit@gmail.com>
2025-09-21 11:18:49 +0530

    Updated backend with CORS, face embedding, etc.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
-from app.routers import auth,superadmin, administrator, teacher, classes, attendance
-
-# Create tables in dev only (disable in production, use Alembic instead)
-# Base.metadata.create_all(bind=engine)
+from app.routers import auth, superadmin, administrator, teacher, classes, attendance
 
 # Initialize app
 app = FastAPI(
     title="Smart Attendance API",
     description="Backend for Attendance System with SuperAdmins, Schools, Teachers, and Students",
     version="1.0.0"
+)
+
+# ✅ Allow frontend requests (Flutter Web / Desktop running on localhost:55304)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],          # allow all methods (GET, POST, PUT, DELETE)
+    allow_headers=["*"],          # allow all headers (Authorization, Content-Type, etc.)
 )
 
 # Include routers
